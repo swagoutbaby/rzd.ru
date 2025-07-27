a4513b8301bffee2ba13958282043d6d9e0e5bfa
ChurinDmitriy <mister.dim2017111@gmail.com>
2025-07-28 00:59:09 +0500

    trivial update

--- src/test/java/pages/PageWithEntry.java
+++ src/test/java/pages/PageWithEntry.java
@@ -1,6 +1,5 @@
 package pages;
 
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.cssValue;
@@ -28,7 +27,6 @@ public class PageWithEntry {
 
     public PageWithEntry openPage () {
         open("");
-        Selenide.sleep(3000);
         return this;
     }
 
